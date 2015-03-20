ce31d6ebccb41d9dd2eb02d564edc189da3e8216
NukeDev <admin@forhost.org>
2015-03-21 00:24:40 +0100

    Revert "Revert "First commit, Under Working website.""
    This reverts commit 9597ca72582b00904e0eb2d30b301e8b0b5853d9.

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+
+  <title>ForHost Dev. - Under Working.</title>
+  <link href='https://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet' type='text/css'>
+
+  <style type="text/css">
+
+  h1 {
+     font-family: 'Inconsolata', serif;
+     font-size: 23px;
+  }
+
+  hr {
+      border: 0;
+      height: 1px;
+      background-image: -webkit-linear-gradient(left, rgba(0,0,0,0), rgba(0,0,0,0.75), rgba(0,0,0,0));
+      background-image:    -moz-linear-gradient(left, rgba(0,0,0,0), rgba(0,0,0,0.75), rgba(0,0,0,0));
+      background-image:     -ms-linear-gradient(left, rgba(0,0,0,0), rgba(0,0,0,0.75), rgba(0,0,0,0));
+      background-image:      -o-linear-gradient(left, rgba(0,0,0,0), rgba(0,0,0,0.75), rgba(0,0,0,0));
+  }
+
+  #content {
+     position: absolute;
+     left: 50%;
+     top: 50%;
+     -webkit-transform: translate(-50%, -50%);
+     transform: translate(-50%, -50%);
+  }
+
+
+  </style>
+
+<body>
+  <body background="img/back.jpg">
+    <div>
+      <div id="content">
+        <center><img src="img/logo.png" alt="Temp. Logo"></center>
+          <hr>
+            <center>
+              <h1>The WebSite is currently under working. Please check it back soon.</h1>
+              <code>For any further informations, contact us on dev@forhost.org</code>
+            </center>
+          <hr>
+      </div>
+    </div>
+
+  <!--footer>
+     Copyright 2015 ForHost Dev. - All Rights Reserved.
+  </footer-->
+
+</body>
+</head>
+</html>
